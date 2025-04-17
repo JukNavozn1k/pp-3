3e95baca5a59e110ea36f76ecb1cca2856484031
gleb <deffender1337@gmail.com>
2025-04-17 13:05:32 +0300

    deep parallel

--- main.cpp
+++ main.cpp
@@ -8,13 +8,14 @@ using namespace std;
 using Matrix = vector<vector<int>>;
 
 const int STRASSEN_THRESHOLD = 64;
-
-// Максимальное число потоков, задаётся из main()
-int maxThreads = 4;
+int maxThreads = 4;  // задаётся из main()
 
 // Прототипы
+Matrix standardMultiply(const Matrix& A, const Matrix& B);
 Matrix strassenSequential(const Matrix& A, const Matrix& B);
+Matrix strassenParallelRecursive(const Matrix& A, const Matrix& B, int threadsLeft);
 Matrix strassenParallel(const Matrix& A, const Matrix& B);
+bool areMatricesEqual(const Matrix& A, const Matrix& B);
 
 Matrix generateRandomMatrix(int n) {
     Matrix A(n, vector<int>(n));
@@ -59,12 +60,11 @@ Matrix strassenSequential(const Matrix& A, const Matrix& B) {
         return standardMultiply(A, B);
 
     int m = n / 2;
-    // Блоки A и B
+    // Разбиение на 4 подматрицы
     Matrix A11(m, vector<int>(m)), A12(m, vector<int>(m)),
            A21(m, vector<int>(m)), A22(m, vector<int>(m));
     Matrix B11(m, vector<int>(m)), B12(m, vector<int>(m)),
            B21(m, vector<int>(m)), B22(m, vector<int>(m));
-
     for (int i = 0; i < m; i++)
         for (int j = 0; j < m; j++) {
             A11[i][j] = A[i][j];
@@ -77,7 +77,7 @@ Matrix strassenSequential(const Matrix& A, const Matrix& B) {
             B22[i][j] = B[i + m][j + m];
         }
 
-    // Семь продуктов
+    // 7 продуктов
     Matrix M1 = strassenSequential(add(A11, A22), add(B11, B22));
     Matrix M2 = strassenSequential(add(A21, A22), B11);
     Matrix M3 = strassenSequential(A11, subtract(B12, B22));
@@ -86,38 +86,41 @@ Matrix strassenSequential(const Matrix& A, const Matrix& B) {
     Matrix M6 = strassenSequential(subtract(A21, A11), add(B11, B12));
     Matrix M7 = strassenSequential(subtract(A12, A22), add(B21, B22));
 
-    // Сборка
+    // Сборка C
     Matrix C(n, vector<int>(n));
     for (int i = 0; i < m; i++)
         for (int j = 0; j < m; j++) {
-            C[i][j]                 = M1[i][j] + M4[i][j] - M5[i][j] + M7[i][j];
-            C[i][j + m]             = M3[i][j] + M5[i][j];
-            C[i + m][j]             = M2[i][j] + M4[i][j];
-            C[i + m][j + m]         = M1[i][j] - M2[i][j] + M3[i][j] + M6[i][j];
+            C[i][j]         = M1[i][j] + M4[i][j] - M5[i][j] + M7[i][j];
+            C[i][j + m]     = M3[i][j] + M5[i][j];
+            C[i + m][j]     = M2[i][j] + M4[i][j];
+            C[i + m][j + m] = M1[i][j] - M2[i][j] + M3[i][j] + M6[i][j];
         }
     return C;
 }
 
-// Для передачи в поток
-struct SeqArgs {
+// Аргументы для потокового параллельного вызова
+struct ParArgs {
     const Matrix* A;
     const Matrix* B;
     Matrix* result;
+    int threadsLeft;
 };
-void* seqWorker(void* arg) {
-    auto* a = static_cast<SeqArgs*>(arg);
-    *a->result = strassenSequential(*a->A, *a->B);
+
+// Функция-обёртка для запуска strassenParallelRecursive в потоке
+void* parWorker(void* arg) {
+    auto* p = static_cast<ParArgs*>(arg);
+    *p->result = strassenParallelRecursive(*p->A, *p->B, p->threadsLeft);
     return nullptr;
 }
 
-// Параллельный только на top-level
-Matrix strassenParallel(const Matrix& A, const Matrix& B) {
+// Рекурсивный параллельный Strassen на всех уровнях
+Matrix strassenParallelRecursive(const Matrix& A, const Matrix& B, int threadsLeft) {
     int n = A.size();
-    if (n <= STRASSEN_THRESHOLD || maxThreads <= 1)
+    if (n <= STRASSEN_THRESHOLD || threadsLeft <= 1)
         return strassenSequential(A, B);
 
     int m = n / 2;
-    // Блоки A и B
+    // Разбиение
     Matrix A11(m, vector<int>(m)), A12(m, vector<int>(m)),
            A21(m, vector<int>(m)), A22(m, vector<int>(m));
     Matrix B11(m, vector<int>(m)), B12(m, vector<int>(m)),
@@ -134,7 +137,7 @@ Matrix strassenParallel(const Matrix& A, const Matrix& B) {
             B22[i][j] = B[i + m][j + m];
         }
 
-    // Подготовка 7 задач
+    // Подготовка семи пар (A, B)
     Matrix As[7], Bs[7], Ms[7];
     As[0] = add(A11, A22);       Bs[0] = add(B11, B22);
     As[1] = add(A21, A22);       Bs[1] = B11;
@@ -144,37 +147,41 @@ Matrix strassenParallel(const Matrix& A, const Matrix& B) {
     As[5] = subtract(A21, A11);  Bs[5] = add(B11, B12);
     As[6] = subtract(A12, A22);  Bs[6] = add(B21, B22);
 
-    // Сколько потоков насоздаём (не больше 7, и не больше maxThreads‑1)
-    int spawn = min(maxThreads - 1, 7);
+    // Сколько потоков создаём (не больше 7 и не больше threadsLeft‑1)
+    int spawn = min(threadsLeft - 1, 7);
     pthread_t tids[7];
-    SeqArgs args[7];
+    ParArgs args[7];
 
-    // 1) Создаём spawn потоков
+    // Запускаем первые spawn задач в отдельных потоках
     for (int i = 0; i < spawn; i++) {
-        args[i] = { &As[i], &Bs[i], &Ms[i] };
-        pthread_create(&tids[i], nullptr, seqWorker, &args[i]);
+        args[i] = { &As[i], &Bs[i], &Ms[i], threadsLeft - 1 };
+        pthread_create(&tids[i], nullptr, parWorker, &args[i]);
     }
-    // 2) Остальные задачи в текущем (главном) потоке
+    // Оставшиеся задачи выполняем в текущем потоке (но тоже рекурсивно параллельно)
     for (int i = spawn; i < 7; i++) {
-        Ms[i] = strassenSequential(As[i], Bs[i]);
+        Ms[i] = strassenParallelRecursive(As[i], Bs[i], threadsLeft - 1);
     }
-    // 3) Ждём потоков
+    // Ждём завершения потоков
     for (int i = 0; i < spawn; i++) {
         pthread_join(tids[i], nullptr);
     }
 
-    // Сборка результата
+    // Сборка итоговой матрицы
     Matrix C(n, vector<int>(n));
     for (int i = 0; i < m; i++)
         for (int j = 0; j < m; j++) {
-            C[i][j]                 = Ms[0][i][j] + Ms[3][i][j]
-                                    - Ms[4][i][j] + Ms[6][i][j];
-            C[i][j + m]             = Ms[2][i][j] + Ms[4][i][j];
-            C[i + m][j]             = Ms[1][i][j] + Ms[3][i][j];
-            C[i + m][j + m]         = Ms[0][i][j] - Ms[1][i][j]
-                                    + Ms[2][i][j] + Ms[5][i][j];
+            C[i][j]         = Ms[0][i][j] + Ms[3][i][j]
+                            - Ms[4][i][j] + Ms[6][i][j];
+            C[i][j + m]     = Ms[2][i][j] + Ms[4][i][j];
+            C[i + m][j]     = Ms[1][i][j] + Ms[3][i][j];
+            C[i + m][j + m] = Ms[0][i][j] - Ms[1][i][j]
+                            + Ms[2][i][j] + Ms[5][i][j];
         }
     return C;
+}
+
+Matrix strassenParallel(const Matrix& A, const Matrix& B) {
+    return strassenParallelRecursive(A, B, maxThreads);
 }
 
 bool areMatricesEqual(const Matrix& A, const Matrix& B) {
@@ -193,7 +200,7 @@ int main(int argc, char* argv[]) {
     }
     cout << "Using up to " << maxThreads << " threads\n";
 
-    int n = 1024;
+    int n = 2048;
     Matrix A = generateRandomMatrix(n);
     Matrix B = generateRandomMatrix(n);
 
